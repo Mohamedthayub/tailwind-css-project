85c7c74bf1f419b59e498dc2bdb822f8da0a5a1a
buyath <buyaththayub@gmail.com>
2025-06-20 21:04:29 +0000

    eccomerce html done

---
+++ ecommerce-product-page-main/tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'], // 👈 Add this line
+      },
+    },
+  },
+  plugins: [],
+};
